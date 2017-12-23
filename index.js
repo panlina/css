c1a0a44aa41f2a7aff384a426106b5b8a47a5b0f
Jack Lu <jacklu@jacklu.me>
2017-12-23 21:07:58 +0800

    +adapt

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var what = require('css-what');
 module.exports = function (css) {
 	var css = what(css);
 	var css = css[0];
-	return function (node) {
+	return function (node, adapt) {
 		return css.reduce(function (node, filter) {
 			switch (filter.type) {
-				case 'child': return [].concat.apply([], node.map(function (n) { return n.children; }));
+				case 'child': return [].concat.apply([], node.map(function (n) { return adapt.child(n); }));
 				case 'descendant': return [].concat.apply([], node.map(descendant));
 				default: return node.filter(match(filter));
 			}
 		}, descendant(node));
+		function match(filter) {
+			switch (filter.type) {
+				case 'universal': return function () { return true; };
+				case 'tag': return function (node) { return adapt.type(node) == 'element' && adapt.name(node) == filter.name; };
+			}
+		}
+		function descendant(node) {
+			return [].concat.apply([node], adapt.child(node).map(descendant));
+		}
 	};
 };
-function match(filter) {
-	switch (filter.type) {
-		case 'universal': return function () { return true; };
-		case 'tag': return function (node) { return node.type == 'element' && node.name == filter.name; };
-	}
-}
-function descendant(node) {
-	return [].concat.apply([node], node.children.map(descendant));
-}
