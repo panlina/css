3717ffd62012e9f7614781dca6611456555f1a35
Jack Lu <jacklu@jacklu.me>
2017-12-23 21:40:31 +0800

    +match

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-var what = require('css-what');
-module.exports = function (css) {
-	var css = what(css);
-	var css = css[0];
-	return function (node, adapt) {
-		return css.reduce(function (node, filter) {
-			switch (filter.type) {
-				case 'child': return [].concat.apply([], node.map(function (n) { return adapt.child(n); }));
-				case 'descendant': return [].concat.apply([], node.map(descendant));
-				default: return node.filter(match(filter));
-			}
-		}, descendant(node));
-		function match(filter) {
-			switch (filter.type) {
-				case 'universal': return function () { return true; };
-				case 'tag': return function (node) { return adapt.type(node) == 'element' && adapt.name(node) == filter.name; };
-			}
-		}
-		function descendant(node) {
-			return [].concat.apply([node], adapt.child(node).map(descendant));
-		}
-	};
-};
+exports.select = require('./select.js');
+exports.match = require('./match.js');
---
+++ match.js
@@ -0,0 +1,33 @@
+var select = require('./select.js');
+var SPECIFICITY = require('specificity');
+module.exports = function (css) {
+	return function (element, adapt) {
+		var ruleelement =
+			css.map(function (rule) {
+				return {
+					rule: rule,
+					element: select(rule.selector)(element, adapt)
+				};
+			});
+		var elementrule = {};
+		var id = 0;
+		for (var i in ruleelement)
+			ruleelement[i].element.forEach(function (element, j) {
+				if (!('$id' in element))
+					elementrule[element.$id = id++] = {
+						element: element,
+						rule: []
+					};
+				elementrule[element.$id].rule.push(ruleelement[i].rule);
+			});
+		for (var id in elementrule) {
+			var rule = elementrule[id].rule;
+			rule.sort(function (r, s) {
+				return SPECIFICITY.compare(r.selector, s.selector);
+			});
+			var style = rule.map(function (rule) { return rule.style; });
+			elementrule[id].style = style;
+		}
+		return elementrule;
+	};
+}
